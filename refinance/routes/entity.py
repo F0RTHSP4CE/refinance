d2e654e3a02ad9296dc0016e4b90354c726a5976
Mike_Went <git@meew.me>
2024-03-31 05:55:00 +0400

    service → entity_service

--- refinance/routes/entity.py
+++ refinance/routes/entity.py
@@ -11,24 +11,34 @@ entity_router = APIRouter(prefix="/entities", tags=["Entities"])
 
 
 @entity_router.post("/", response_model=EntitySchema)
-def create_entity(entity: EntityCreateSchema, service: EntityService = Depends()):
-    return service.create(entity)
+def create_entity(
+    entity: EntityCreateSchema,
+    entity_service: EntityService = Depends(),
+):
+    return entity_service.create(entity)
 
 
 @entity_router.get("/{entity_id}", response_model=EntitySchema)
-def read_entity(entity_id: int, service: EntityService = Depends()):
-    return service.get(entity_id)
+def read_entity(
+    entity_id: int,
+    entity_service: EntityService = Depends(),
+):
+    return entity_service.get(entity_id)
 
 
 @entity_router.get("/", response_model=list[EntitySchema])
-def read_entities(skip: int = 0, limit: int = 10, service: EntityService = Depends()):
-    return service.get_all(skip=skip, limit=limit)
+def read_entities(
+    skip: int = 0,
+    limit: int = 10,
+    entity_service: EntityService = Depends(),
+):
+    return entity_service.get_all(skip=skip, limit=limit)
 
 
 @entity_router.patch("/{entity_id}", response_model=EntitySchema)
 def update_entity(
     entity_id: int,
     entity_update: EntityUpdateSchema,
-    service: EntityService = Depends(),
+    entity_service: EntityService = Depends(),
 ):
-    return service.update(entity_id, entity_update)
+    return entity_service.update(entity_id, entity_update)
