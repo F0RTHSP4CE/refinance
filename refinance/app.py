f74f37be64c2c6c57dae63d1d7ce1cb3ab443cca
Mike_Went <git@meew.me>
2024-04-01 02:38:48 +0400

    remove async

--- refinance/app.py
+++ refinance/app.py
@@ -11,7 +11,7 @@ app = FastAPI(title=config.app_name)
 
 
 @app.exception_handler(ApplicationError)
-async def application_exception_handler(request: Request, exc: ApplicationError):
+def application_exception_handler(request: Request, exc: ApplicationError):
     return JSONResponse(
         status_code=500, content={"error_code": exc.error_code, "error": exc.error}
     )
