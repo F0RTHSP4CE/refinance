a898a96ba4ebd592f3e0ee40ffddfc86b4eebc3d
Mike_Went <git@meew.me>
2024-04-01 06:05:15 +0400

    add automatic __repr__ to models

--- refinance/models/base.py
+++ refinance/models/base.py
@@ -2,6 +2,7 @@
 
 from typing import Optional
 
+from sqlalchemy.inspection import inspect
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 
 
@@ -12,3 +13,15 @@ class BaseModel(DeclarativeBase):
     # everything should have an id and a comment
     id: Mapped[int] = mapped_column(primary_key=True)
     comment: Mapped[Optional[str]]
+
+    def __repr__(self):
+        """Automatically generate beautiful __repr__ of a database object (chatgpt4 generated snippet)"""
+        model_name = self.__class__.__name__
+        attr_strs = []
+        for attr, column in inspect(self.__class__).columns.items():
+            value = getattr(self, attr)
+            attr_strs.append(
+                f"{attr}={value!r}"
+            )  # !r calls repr() on the value to get a nice string representation
+        attr_str = ", ".join(attr_strs)
+        return f"<{model_name}({attr_str})>"
