4b33665bebb26a39aa455fc9adc868737d99711d
Mike_Went <git@meew.me>
2024-06-07 14:39:07 +0400

    improve token errors

--- refinance/errors/token.py
+++ refinance/errors/token.py
@@ -6,4 +6,10 @@ from refinance.errors.base import ApplicationError
 class TokenInvalid(ApplicationError):
     http_code = 403
     error_code = 3001
-    error = "Token is invalid or missing"
+    error = "Token is invalid"
+
+
+class TokenMissing(ApplicationError):
+    http_code = 403
+    error_code = 3002
+    error = "Token is missing"
